1652ddb652aeba55d2501d7e1cd2559f6f7651ed
Jérémie Perreault <1751707@etu.cegep-lanaudiere.qc.ca>
2023-02-03 16:12:11 -0500

    #10 etablir route
    route api

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,37 @@
-let express = require('express');
-let userController = require('./controllers/UserController');
+const express = require('express')
+const userController = require('./controllers/UserController')
 
-let router = express.Router();
-router.route('/user/create').get(userController.createUser)
-module.exports = router;
+const router = express.Router()
+router
+  .route('/user')
+  .get(userController.getUser)
+  .post(userController.createUser)
+  .put(userController.updateUser)
+  .delete(userController.deleteUser)
+
+router
+  .route('/tournament')
+  .get(tournamentController.getTournament)
+  .post(tournamentController.createTournament)
+  .put(tournamentController.updateTournament)
+  .delete(tournamentController.deleteTournament)
+
+router
+  .route('/match')
+  .get(matchController.getMatch)
+  .post(matchController.createMatch)
+  .put(matchController.updateMatch)
+  .delete(matchController.deleteMatch)
+
+router
+  .route('/player')
+  .get(playerController.getPlayer)
+  .post(playerController.createPlayer)
+  .put(playerController.updatePlayer)
+  .delete(playerController.deletePlayer)
+
+router
+    .route('/')
+
+
+module.exports = router
