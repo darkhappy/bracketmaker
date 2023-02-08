87d9f1de58b2273a5d2dadc1a44136278e58af9b
Louis-Félix Veillette <2038026@etu.cegep-lanaudiere.qc.ca>
2023-02-08 12:58:32 -0500

    yay

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -141,9 +141,9 @@ function getToken(req, res) {
 
 function generate_token(length){
   var a = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".split("");
-  var b = [];  
-  for (var i=0; i<length; i++) {
-    var j = (Math.random() * (a.length-1)).toFixed(0);
+  let b = [];  
+  for (let i=0; i<length; i++) {
+    let j = (Math.random() * (a.length-1)).toFixed(0);
     b[i] = a[j];
   }
   return b.join("");
