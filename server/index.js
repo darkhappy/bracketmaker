5805cbcca7848669c88694cfe73d97aa8132453d
gabriel dufresne <gabrielduf@hotmail.com>
2023-02-08 11:05:38 -0500

    fonction getUser, jwtToken et dotEnv

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config()
 
 mongoose.set('strictQuery', false);
 mongoose.connect('mongodb://127.0.0.1:27017/test', { useNewUrlParser: true, useUnifiedTopology: true })
 
 const app = express();
-let router = require('./routes'); 
+let router = require('./routes');
+const dotenv = require("dotenv");
 app.use(express.json());
 app.use(router);
 app.listen(3000, () => {
--- server/middlewares/UserMiddleware.js
+++ server/middlewares/UserMiddleware.js
@@ -1,2 +1,19 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
+
+const isAuth = (res, req, next) => {
+    let token = req.cookies['SESSIONID'] ?? null;
+    if(!token){
+        return res.sendStatus(401);
+    }
+
+    jwt.verify(token, "cle-tres-secrete", (err, payload) =>{
+        if(err){
+            return res.sendStatus(401);
+        }
+        req.payload = payload;
+        next();
+    });
+}
+
+module.exports = { isAuth };
 
