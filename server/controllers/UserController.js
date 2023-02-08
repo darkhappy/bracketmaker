821c268271473785ff28a6cafd7459edde427424
Insane hvh <1751707@etu.cegep-lanaudiere.qc.ca>
2023-02-07 23:15:24 -0500

    auth-register(#22) : creation d'utilisateur
    check si le username/email est use

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,6 +1,8 @@
 const User = require('../models/User')
+const bcrypt = require('bcrypt')
 
 function getAllUsers (req, res) {
+  // eslint-disable-next-line n/handle-callback-err
   User.find().exec((err, users) => {
     res.json(users)
   })
@@ -11,12 +13,35 @@ function getUser (req, res) {
 }
 
 function createUser (req, res) {
-  const user = new User(req.body)
-  user.save().then(() => {
-    console.log('User created')
-    return res.sendStatus(204)
-  }).catch((err) => {
-    return res.status(500).json(err)
+  User.find({ username: req.body.username }).exec((err, users) => {
+    if (err || users.length > 0) {
+      return res.status(409).json({ message: 'Username already exists' })
+    } else {
+      User.find({ email: req.body.email }).exec((err, users) => {
+        if (err || users.length > 0) {
+          return res.status(409).json({ message: 'Email already in use' })
+        } else {
+          const user = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: bcrypt.hashSync(req.body.password, 10),
+            token: '',
+            // token = createToken()
+            isVerified: false,
+            firstname: '',
+            lastname: '',
+            about: '',
+            avatar: ''
+          })
+          user.save().then(() => {
+            console.log('User created')
+            return res.sendStatus(204)
+          }).catch((err) => {
+            return res.status(500).json(err)
+          })
+        }
+      })
+    }
   })
 }
 
@@ -36,4 +61,8 @@ function deleteUser (req, res) {
   // todo: a faire
 }
 
-module.exports = { getAllUsers, createUser, updateUser, getUser, deleteUser }
+function createToken (user) {
+  // todo: a faire
+}
+
+module.exports = { getAllUsers, createUser, updateUser, getUser, deleteUser, createToken }
