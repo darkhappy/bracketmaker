b0d5ddca884b2fc4fdd29640de19b93d5b5965e0
Jean-Philippe <github@darkhappy.ca>
2023-02-14 15:18:38 -0500

    change email/user done

--- client/src/app/modules/user/components/forms/change-username/change-username.component.ts
+++ client/src/app/modules/user/components/forms/change-username/change-username.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input } from "@angular/core";
 import { AuthService } from "@data/services/auth.service";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
+import { MatDialogRef } from "@angular/material/dialog";
 
 @Component({
   selector: 'app-change-username',
@@ -7,9 +10,39 @@ import { AuthService } from "@data/services/auth.service";
   styleUrls: ['./change-username.component.scss']
 })
 export class ChangeUsernameComponent {
-  constructor(private authService: AuthService) { }
+  formChangeUsername!: FormGroup;
+
+  constructor(private authService: AuthService,
+              private formBuilder: FormBuilder,
+              private router: Router,
+              private dialogRef: MatDialogRef<ChangeUsernameComponent>) { }
+
+
+  ngOnInit(): void {
+    this.formChangeUsername = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  onClose() {
+    this.dialogRef.close(false);
+  }
 
   onSubmit() {
+    if (this.formChangeUsername.invalid) {
+      return;
+    }
+
+    this.authService.changeUsername(this.formChangeUsername.value).subscribe({
+      next: () => {
+        alert("Username changed successfully!");
+        this.dialogRef.close(true);
+      },
+      error: (error) => {
+        alert("There was an error changing the password. Please try again later.");
+      }
+    });
 
   }
 }
