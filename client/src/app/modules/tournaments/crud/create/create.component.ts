665521ecc80d71d2308c2cac54004f6c74a44c10
Gabriel Dufresne <gabrielduf@hotmail.com>
2023-02-15 17:35:57 -0500

    (tournament crud create(#49): début du formulaire de création de tournois

---
+++ client/src/app/modules/tournaments/crud/create/create.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-create',
+  templateUrl: './create.component.html',
+  styleUrls: ['./create.component.scss']
+})
+export class CreateComponent {
+  // @ts-ignore
+  formCreate : FormGroup;
+  constructor(private fb: FormBuilder) { }
+  ngOnInit(): void {
+    this.formCreate = this.fb.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      date: ['', Validators.required],
+      bracket_type: ['', Validators.required],
+      visibility: ['', Validators.required],
+      location: ['', Validators.required],
+      game: ['', Validators.required],
+      player: ['', Validators.required],
+    });
+  }
+  onSubmit(){
+
+  }
+}
--- client/src/app/modules/tournaments/tournaments.module.ts
+++ client/src/app/modules/tournaments/tournaments.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CreateComponent } from './crud/create/create.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [
+    CreateComponent
+  ],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule
+    ]
 })
 export class TournamentsModule { }
