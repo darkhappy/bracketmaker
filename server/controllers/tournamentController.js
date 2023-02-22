a3aae2fab079d4cfb08d2758d7c217dd5b654ed9
gabriel dufresne <gabrielduf@hotmail.com>
2023-02-22 12:17:04 -0500

    tournaments-crud-create: modification demanddé par louis

--- server/controllers/tournamentController.js
+++ server/controllers/tournamentController.js
@@ -1,23 +1,38 @@
 const Tournament = require('../models/tournament')
 const User = require('../models/User')
 const moment = require("moment");
+const middleware = require("../middlewares/UserMiddleware");
+const bcrypt = require("bcrypt");
+const Match = require("../models/match");
 
 function getTournament (req, res) {
   // todo: a faire
 }
 
 async function createTournament(req, res) {
-    const user = await User.findById(req.body.organiserID).exec()
+    const user = await User.findById(req.payload.id).exec()
     if (!user)
-        return res.status(409).json({message: 'id organisateur non valide'})
-    if (!moment("06/22/2015", "MM/DD/YYYY", true).isValid())
+        return res.status(401).json({message: 'id organisateur non valide'})
+    if (moment(req.body.date, "MM/DD/YYYY", false).isValid())
         return res.sendStatus(401).json({message: 'date non valide'})
     if (req.body.visibility !== 'public' && req.body.visibility !== 'private' && req.body.visibility !== 'unlisted')
         return res.sendStatus(401).json({message: 'visibilité non valide'})
     if (req.body.bracket_type !== 'Simple' && req.body.bracket_type !== 'Double' && req.body.bracket_type !== 'Round Robin')
         return res.sendStatus(401).json({message: 'type de tournoi non valide'})
 
-    let tournament = new Tournament(req.body)
+    const tournament = new Tournament({
+        name : req.body.name,
+        description : req.body.description,
+        bracket_type : req.body.bracket_type,
+        category : req.body.category,
+        date : req.body.date,
+        visibility: req.body.visibility,
+        location: req.body.location,
+        game: req.body.game,
+        players : req.body.players,
+        organizer_id : req.payload.id
+    })
+
     tournament.save().then(() => {
         return res.sendStatus(204)
     }).catch(() => {
