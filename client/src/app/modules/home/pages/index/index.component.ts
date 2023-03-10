c2344acc227e3165c469bec386fbf89cae969037
answr42Hub <raphthearrow@gmail.com>
2023-03-10 16:02:30 -0500

    commentaires pour la page d'accueil

--- client/src/app/modules/home/pages/index/index.component.ts
+++ client/src/app/modules/home/pages/index/index.component.ts
@@ -9,21 +9,30 @@ import {AuthService} from "@data/services/auth.service";
 })
 export class IndexComponent {
 
+  // images d'arrière plan
   images = ["bg-hockey", "bg-chess", "bg-soccer", "bg-gaming", "bg-volley"];
   background: string = "bg-hockey";
 
   constructor(private router: Router, private authService: AuthService) {
   }
 
+  // on choisit une image d'arrière plan au hasard
   ngOnInit(): void {
     const index = Math.floor(Math.random() * this.images.length);
     this.background = this.images[index];
   }
 
+  // on vérifie si l'utilisateur est connecté
   userConnected(): Boolean {
     return this.authService.getUserId() !== null;
   }
 
+  /*
+  * on redirige l'utilisateur vers la page de connexion
+  * si il n'est pas connecté
+  * sinon on redirige vers la page de jeu
+  * param: path: string
+   */
   navigate(path: string) {
     this.router.navigate([path]);
   }
